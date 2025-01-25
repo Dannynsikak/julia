d8c3aba7e97ed3f10b5071995eca9ac7dab7f217
Dannynsikak <nsikakdanny11@gmail.com>
2025-01-26 00:26:35 +0100

    fondling with julia

---
+++ variable.jl
@@ -0,0 +1,21 @@
+# assign values to variables
+x = 42  # integer
+pi = 3.14  # floating-point number
+name = "Julia" # string
+
+# Tip: Use meaningful variable names so your code is easy to understand.
+
+print(12)
+print(12)
+println(12)
+println(12)
+
+# string interpolation to easily print variables and values within strings
+name = "Alice"
+age = 30
+# Simple interpolation
+message = "Hi, I'm $name and I'm $age years old."
+println(message)
+# interpolating expressions
+caalculation = "5 + 10 equals $(5 + 10)."
+println(caalculation)
