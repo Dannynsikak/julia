204479c24ed2edb76eb64d6bcaa7311b15b05de5
Dannynsikak <nsikakdanny11@gmail.com>
2025-01-26 16:21:30 +0100

    added few julia functions

---
+++ collections.jl
@@ -0,0 +1,6 @@
+# A simple array of numbers 
+numbers = [1, 2, 3, 4, 5]
+println(numbers) 
+# An array of names
+names = ["Alice", "Bob", "Charlie"]
+println(names)  # Output: ["Alice", "Bob", "Charlie"]
